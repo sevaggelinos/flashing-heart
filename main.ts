6c71128297c1ea687e0c42478eeb336c2011a094
sevaggelinos <sevaggelinos@gmail.com>
2026-03-24 17:32:54 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
