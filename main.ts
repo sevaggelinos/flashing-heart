896acb0a964160063c91195f20089bc2359b44ad
sevaggelinos <sevaggelinos@gmail.com>
2026-03-24 23:31:07 +0200

    Update main.blocks, main.ts, _history

--- main.ts
+++ main.ts
@@ -1,31 +1,33 @@
-input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 10; index++) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . # . # .
-            # # # # #
-            # # # # #
-            . # # # .
-            . . # . .
-            `)
-        if (input.buttonIsPressed(Button.B)) {
-            break;
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        while (true) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . # . # .
+                # # # # #
+                # # # # #
+                . # # # .
+                . . # . .
+                `)
+            basic.pause(300)
+            if (input.buttonIsPressed(Button.B)) {
+                break;
+            }
         }
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        pins.digitalWritePin(DigitalPin.P0, 1)
     }
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    pins.digitalWritePin(DigitalPin.P0, 1)
 })
